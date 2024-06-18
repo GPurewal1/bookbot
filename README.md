fc2dd5821d7e87aa23bd46108f337ff1da7dff23
GPurewal1 <gurpreet.purewal3@gmail.com>
2024-06-18 17:44:07 -0400

    add word and character count report feature

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # bookbot
 
-BookBot is my first project!
+Program takes a file with text from a book and prints a report of total words and character count.
+File path can be modified on line 2 to feed program the book.
---
+++ main.py
@@ -0,0 +1,42 @@
+def main():
+    file_path = 'books/frankenstein.txt'
+    with open(file_path) as f:
+        book_text = f.read()
+        word_count_total = word_count(book_text)
+        character_count_list = character_count(book_text)
+
+        print(f"--- Begin report of {file_path} ---")
+        print(f"{word_count_total} words found in the document")
+        print(" ")
+        for char in character_count_list:
+            print(f"The '{char['char']}' character was found {char['num']} times")
+        print("--- End report ---")
+
+def sort_on(d):
+    return d["num"]
+
+def word_count(text):
+    words_list = text.split()
+    return len(words_list)
+
+def character_count(text):
+    character_dict = {}
+    az_character_list_sorted = []
+    lowered_text = text.lower()
+
+    for char in lowered_text:
+        if char in character_dict:
+            character_dict[char] += 1
+        elif char.isalpha():
+            character_dict[char] = 1
+
+    for key,value in character_dict.items():
+        az_character_list_sorted.append({"char":key, "num":value})
+    
+    az_character_list_sorted.sort(reverse=True, key=sort_on)
+    return az_character_list_sorted
+        
+
+
+
+main()
